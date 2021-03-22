68084b7fefa79048c7456703734b6efd43caf942
Timur <timurbabs@gmail.com>
2021-03-22 15:08:12 +0300

    Edited

--- Trash/Stalker_bot/main.py
+++ Trash/Stalker_bot/main.py
@@ -1,10 +1,9 @@
 import telebot
 import parsing
+from data import API_TOKEN
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-API_TOKEN = '1752094454:AAFRDIxO135xlcRAbuJ36VpzsilSCXfPuco'
 bot = telebot.TeleBot(API_TOKEN)
-more_data = None
 
 
 @bot.message_handler(commands=['start'])
@@ -12,30 +11,32 @@ def start(message):
     keyboard = [[InlineKeyboardButton("ТЧ", callback_data='1'), InlineKeyboardButton("ЧН", callback_data='2'),
                  InlineKeyboardButton("ЗП", callback_data='3'), InlineKeyboardButton("ВК", callback_data='4')]]
     markup = InlineKeyboardMarkup(keyboard)
-    bot.send_message(chat_id=message.chat.id, text='Choose your joke:', reply_markup=markup)
+    bot.send_message(chat_id=message.chat.id, text='Choose your joke.', reply_markup=markup)
+
+
+more_data = None
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
     global more_data
     try:
-        bot.answer_callback_query(callback_query_id=call.id)
         if call.data in ('1', '2', '3', '4'):
-            bot.send_message(chat_id=call.message.chat.id, text=get_joke(call.data))
-            keyboard = [
-                [InlineKeyboardButton("More", callback_data='0'), InlineKeyboardButton("Another one", callback_data='9')]]
+            keyboard = [[InlineKeyboardButton("More", callback_data='0'),
+                         InlineKeyboardButton("Another one", callback_data='9')]]
             markup = InlineKeyboardMarkup(keyboard)
-            bot.send_message(chat_id=call.message.chat.id, text='Choose option:', reply_markup=markup)
+            bot.send_message(chat_id=call.message.chat.id, text=get_joke(call.data), reply_markup=markup)
+            bot.answer_callback_query(callback_query_id=call.id)
             more_data = call.data
         elif call.data == "0":
-            joke = get_joke(more_data)
-            bot.send_message(chat_id=call.message.chat.id, text=joke)
-            keyboard = [
-                [InlineKeyboardButton("More", callback_data='0'), InlineKeyboardButton("Another one", callback_data='9')]]
+            keyboard = [[InlineKeyboardButton("More", callback_data='0'),
+                         InlineKeyboardButton("Another one", callback_data='9')]]
             markup = InlineKeyboardMarkup(keyboard)
-            bot.send_message(chat_id=call.message.chat.id, text='Choose option:', reply_markup=markup)
+            bot.send_message(chat_id=call.message.chat.id, text=get_joke(more_data), reply_markup=markup)
+            bot.answer_callback_query(callback_query_id=call.id)
         elif call.data == "9":
             start(call.message)
+            bot.answer_callback_query(callback_query_id=call.id)
     except Exception:
         pass
 
--- Trash/Stalker_bot/parsing.py
+++ Trash/Stalker_bot/parsing.py
@@ -1,8 +1,7 @@
 import requests
 import bs4
 import random
-
-URL = 'https://stalker-game.fandom.com/ru/wiki/%D0%90%D0%BD%D0%B5%D0%BA%D0%B4%D0%BE%D1%82%D1%8B'
+from data import URL
 
 
 def get_html(url):
@@ -17,11 +16,11 @@ def get_data(Chapter: str):
     jokes_poem = jokes_main_class.find_all(class_='poem')
     joke = None
     if Chapter == "ТЧ":
-        joke = jokes_poem[random.randint(1, 34)]
+        joke = jokes_poem[random.randint(0, 34)]
     elif Chapter == "ЧН":
-        joke = jokes_poem[random.randint(24, 65)]
+        joke = jokes_poem[random.randint(35, 65)]
     elif Chapter == "ЗП":
-        joke = jokes_poem[random.randint(51, 114)]
+        joke = jokes_poem[random.randint(66, 114)]
     elif Chapter == "ВК":
-        joke = jokes_poem[random.randint(51, 127)]
+        joke = jokes_poem[random.randint(115, 127)]
     return joke.get_text()
