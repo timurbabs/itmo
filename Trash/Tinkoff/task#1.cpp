ac93a9687bb8b09a580fd9bf0ab8fa66276c66f9
Timur Baburin <timurbabs@gmail.com>
2021-08-09 10:02:58 +0300

    Minor fixes

--- Trash/Tinkoff/task#1.cpp
+++ Trash/Tinkoff/task#1.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-	int a, b, x, y;
-	cin >> a >> b >> x >> y;
+    int a, b, x, y;
+    cin >> a >> b >> x >> y;
 
     if (a == x) {
         cout << b << " " << a << " " << x << " " << y;
